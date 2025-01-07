8e321bdd91813fc67cbbfb9101c9d059428ebf8f
Abror <106076418+Abdualiev01@users.noreply.github.com>
2025-01-07 11:46:41 +0100

    deps: install svgr

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,8 @@ import { Container } from "@/components";
 export default function Home() {
   return (
     <Container>
-      <h1>Bismillahir Rahmanir Raheem</h1>
+      {/* <h1>Bismillahir Rahmanir Raheem</h1> */}
+      <h2 className="text-[26px] font-bold text-left mt-[100px] mb-10">Our Services</h2>
     </Container>
   );
 }
