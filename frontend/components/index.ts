a2dd61324643eb2f71bdd57dd12b7ddbc1919156
Abror <106076418+Abdualiev01@users.noreply.github.com>
2025-01-09 14:31:40 +0100

    feat: add OrderSummary component

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -2,3 +2,5 @@ export { default as Container } from './Container';
 export { default as Footer } from './Footer';
 export { default as LoginForm } from './LoginForm';
 export { default as SignupForm } from './SignupForm';
+export { default as CartItem } from './CartItem';
+export { default as OrderSummary } from './OrderSummary';
